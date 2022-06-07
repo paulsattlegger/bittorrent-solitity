0bea23984ee26363e619ce3f0ff9657e5206b915
Paul Florian Sattlegger <paul.sattlegger@student.tuwien.ac.at>
2022-06-07 11:39:46 +0200

    Add "cheap" announce to middleware

--- middleware/routes/index.js
+++ middleware/routes/index.js
@@ -12,7 +12,7 @@ function toCompact(addr, port) {
   const buf = Buffer.allocUnsafe(6);
   buf.writeUint32BE(addr, 0);
   buf.writeUint16BE(port, 4);
-  return buf;
+  return Web3.utils.toHex(buf);
 }
 
 // https://stackoverflow.com/a/8064441
@@ -26,7 +26,7 @@ function fromProprietary(encoded) {
       buf.write(encoded[j++], i, 'ascii');
     }
   }
-  return buf;
+  return Web3.utils.toHex(buf);
 }
 
 function toState(state) {
@@ -36,55 +36,86 @@ function toState(state) {
   else return '0';
 }
 
-router.get('/announce', async function (req, res) {
-  console.log(`[HTTP] Request from ${req.header('User-Agent')} at ${req.socket.remoteAddress}:`)
-  console.log(req.query);
-  const result = {};
-
-  if (ip.isV4Format(req.socket.remoteAddress)) {
-    const infoHash = fromProprietary(req.query['info_hash']);
-    const peer = [
-      fromProprietary(req.query['peer_id']),
-      toCompact(ip.toLong(req.socket.remoteAddress), +req.query.port),
-      toState(req.query['event']),
-      req.query['uploaded'],
-      req.query['downloaded'],
-      req.query['left'],
-      '0'
-    ];
-    console.log(`[Contract] Announce ${req.query['info_hash']} with peer:`);
-    console.log(peer);
-
-    // noinspection JSUnresolvedFunction
-    tracker.methods.announce(infoHash, peer).estimateGas().then(
-      (gasAmount) => {
-        // noinspection JSUnresolvedFunction
-        tracker.methods.announce(infoHash, peer).send({
-          from: process.env.SENDER_ADDRESS,
-          gas: gasAmount
-        }).on('receipt', receipt => {
-          console.log('[Contract] Receipt:')
-          console.log(receipt);
-        });
+function announce(infoHash, peer) {
+  // noinspection JSUnresolvedFunction
+  tracker.methods.announce(infoHash, peer).estimateGas().then(
+    (gasAmount) => {
+      // noinspection JSUnresolvedFunction
+      tracker.methods.announce(infoHash, peer).send({
+        from: process.env.SENDER_ADDRESS,
+        gas: gasAmount
+      }).on('receipt', receipt => {
+        console.log('[Contract] Receipt:')
+        console.log(receipt);
       });
+    });
+}
 
-    const peers = await tracker.methods.peers(infoHash).call();
-    result['complete'] = peers.filter(p => +p.left === 0).length;
-    result['incomplete'] = peers.length - result['complete'];
-    result['peers'] = Buffer.concat(peers.map(p => Buffer.from(Web3.utils.hexToBytes(p.compact))).slice(0, +req.query['numwant']));
-    result['interval'] = +(await tracker.methods.interval().call());
+router.get('/announce', async function (req, res) {
+    console.log(`[HTTP] Request from ${req.header('User-Agent')} at ${req.socket.remoteAddress}:`)
+    console.log(req.query);
+    const result = {};
 
-    if (+req.query['compact'] === 0) {
-      result['warning reason'] = 'Tracker supports only compact peer list format';
+    if (ip.isV4Format(req.socket.remoteAddress)) {
+      const infoHash = fromProprietary(req.query['info_hash']);
+      const peerId = fromProprietary(req.query['peer_id']);
+      const peer = [
+        peerId,
+        toCompact(ip.toLong(req.socket.remoteAddress), +req.query.port),
+        toState(req.query['event']),
+        req.query['uploaded'],
+        req.query['downloaded'],
+        req.query['left'],
+        '0'
+      ];
+
+      const timeout = +(await tracker.methods.timeout().call());
+      const peers = await tracker.methods.peers(infoHash).call();
+      const oldPeerId = peers.find(p => +p.updated + timeout <= Date.now() / 1000)?.peerId;
+      const containsPeerId = !!peers.find(p => p.peerId === peerId);
+
+      if (!containsPeerId && oldPeerId) {
+        console.log(`[Contract] Announce ${infoHash} with peer (replacing ${oldPeerId}):`);
+        console.log(peer);
+        // noinspection JSUnresolvedFunction
+        tracker.methods.announce(infoHash, oldPeerId, peer).estimateGas().then(
+          (gasAmount) => {
+            // noinspection JSUnresolvedFunction
+            tracker.methods.announce(infoHash, oldPeerId, peer).send({
+              from: process.env.SENDER_ADDRESS,
+              gas: gasAmount
+            }).on('receipt', receipt => {
+              console.log('[Contract] Receipt:')
+              console.log(receipt);
+            }).on('error', () => {
+              console.log(`[Contract] Announce ${infoHash} with peer:`);
+              console.log(peer);
+              announce(infoHash, peer);
+            });
+          });
+      } else {
+        console.log(`[Contract] Announce ${infoHash} with peer:`);
+        console.log(peer);
+        announce(infoHash, peer);
+      }
+
+      result['complete'] = peers.filter(p => +p.left === 0).length;
+      result['incomplete'] = peers.length - result['complete'];
+      result['peers'] = Buffer.concat(peers.map(p => Buffer.from(Web3.utils.hexToBytes(p.compact))).slice(0, +req.query['numwant']));
+      result['interval'] = +(await tracker.methods.interval().call());
+
+      if (+req.query['compact'] === 0) {
+        result['warning reason'] = 'Tracker supports only compact peer list format';
+      }
+    } else {
+      result['failure reason'] = 'Tracker supports only IPv4 peers'
     }
-  } else {
-    result['failure reason'] = 'Tracker supports only IPv4 peers'
+    console.log('[HTTP] Response:')
+    console.log(result);
+    res.contentType('text/plain');
+    // noinspection JSCheckFunctionSignatures
+    res.send(bencode.encode(result));
   }
-  console.log('[HTTP] Response:')
-  console.log(result);
-  res.contentType('text/plain');
-  // noinspection JSCheckFunctionSignatures
-  res.send(bencode.encode(result));
-});
+);
 
 module.exports = router;
