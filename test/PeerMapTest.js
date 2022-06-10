8b4df2b39f54f4a88e89e06dcb3b23febfad427d
Paul Florian Sattlegger <paul.sattlegger@student.tuwien.ac.at>
2022-06-10 17:14:55 +0200

    Change peerId to address (i.e., msg.sender) in tests

--- test/PeerMapTest.js
+++ test/PeerMapTest.js
@@ -2,18 +2,16 @@ const {reverts} = require("truffle-assertions")
 
 const PeerMap = artifacts.require("PeerMapMock");
 
-contract("PeerMap", async () => {
-  // qBittorrent 4.2.5.0
-  const peerId1 = web3.utils.asciiToHex("-qB4250-aUKMjQy!7F*Y");
-  const peerId2 = peerId1;
-  // Vuze 2.2.0.0
-  const peerId3 = web3.utils.asciiToHex("-AZ2200-6wfG2wk6wWLc");
+contract("PeerMap", async accounts => {
+  const id1 = accounts[1];
+  const id2 = id1;
+  const id3 = accounts[3];
   // 127.0.0.1:6881
   const compact = "0x7f0000011ae1";
-  // peerId, compact, state, uploaded, downloaded, left, updated
-  const peer1 = [peerId1, compact, "0", "0", "0", "100", "0"];
-  const peer2 = [peerId2, compact, "2", "0", "100", "0", "0"];
-  const peer3 = [peerId3, compact, "0", "0", "0", "100", "0"]
+  // id, compact, state, uploaded, downloaded, left, updated
+  const peer1 = [id1, compact, "0", "0", "0", "100", "0"];
+  const peer2 = [id2, compact, "2", "0", "100", "0", "0"];
+  const peer3 = [id3, compact, "0", "0", "0", "100", "0"]
 
   beforeEach(async () => {
     this.peers = await PeerMap.new();
@@ -24,12 +22,12 @@ contract("PeerMap", async () => {
   });
 
   it("get on non-existing peer should revert", async () => {
-    await reverts(this.peers.get(peerId1));
+    await reverts(this.peers.get(id1));
   });
 
   it("get on existing peer should return peer", async () => {
     await this.peers.update(peer1);
-    assert.deepStrictEqual(await this.peers.get(peerId1), peer1);
+    assert.deepStrictEqual(await this.peers.get(id1), peer1);
   });
 
   it("update on empty map should insert peer", async () => {
@@ -48,34 +46,34 @@ contract("PeerMap", async () => {
   it("update on existing peer should update peer", async () => {
     await this.peers.update(peer1);
     await this.peers.update(peer2);
-    assert.deepStrictEqual(await this.peers.get(peerId1), peer2);
+    assert.deepStrictEqual(await this.peers.get(id1), peer2);
   });
 
   it("exchange on existing peer should not increase length", async () => {
     let length = await this.peers.length();
     await this.peers.update(peer1);
-    await this.peers.exchange(peerId1, peer3);
+    await this.peers.exchange(id1, peer3);
     assert.equal(await this.peers.length() - length, 1);
   });
 
   it("exchange on existing peer should remove index to old peer", async () => {
     await this.peers.update(peer1);
-    await this.peers.exchange(peerId1, peer3);
-    await reverts(this.peers.get(peerId1));
+    await this.peers.exchange(id1, peer3);
+    await reverts(this.peers.get(id1));
   });
 
   it("exchange on existing peer should update peer", async () => {
     await this.peers.update(peer1);
-    await this.peers.exchange(peerId1, peer3);
-    assert.deepEqual(await this.peers.get(peerId3), peer3);
+    await this.peers.exchange(id1, peer3);
+    assert.deepEqual(await this.peers.get(id3), peer3);
   });
 
   it("exists on existing peer should return true", async () => {
     await this.peers.update(peer1);
-    assert.equal(await this.peers.exists(peerId1), true);
+    assert.equal(await this.peers.exists(id1), true);
   });
 
   it("exists on non-existing peer should return false", async () => {
-    assert.equal(await this.peers.exists(peerId1), false);
+    assert.equal(await this.peers.exists(id1), false);
   });
 });
--- test/TrackerTest.js
+++ test/TrackerTest.js
@@ -1,69 +1,117 @@
-const {reverts} = require("truffle-assertions")
+const {reverts, eventEmitted} = require("truffle-assertions")
 
 const Tracker = artifacts.require("Tracker");
 
-contract("Tracker", async () => {
+contract("Tracker", async accounts => {
   const infoHash1 = "0x4eae8dbac2397e30de8bcbb206e269254cbe76be";
-  // qBittorrent 4.2.5.0
-  const peerId1 = web3.utils.asciiToHex("-qB4250-aUKMjQy!7F*Y");
-  // Vuze 2.2.0.0
-  const peerId2 = web3.utils.asciiToHex("-AZ2200-6wfG2wk6wWLc");
+  const id1 = accounts[1];
+  const id2 = accounts[2];
+  const empty = "0x0000000000000000000000000000000000000000";
   // 127.0.0.1:6881
   const compact = "0x7f0000011ae1";
-  // peerId, compact, state, uploaded, downloaded, left, updated
-  const peer1 = [peerId1, compact, "0", "0", "0", "100", "0"];
-  const peer2 = [peerId2, compact, "0", "0", "0", "100", "0"];
+  // id, compact, state, uploaded, downloaded, left, updated
+  const peer1 = [id1, compact, "0", "0", "0", "100", "0"];
+  const peer2 = [id2, compact, "0", "0", "0", "100", "0"];
 
   beforeEach(async () => {
     this.tracker = await Tracker.new();
   });
 
   it("announce should add torrent", async () => {
-    await this.tracker.announce(infoHash1, peer1);
+    await this.tracker.announce(infoHash1, peer1, {from: id1});
     const [first] = await this.tracker.torrents();
     assert.equal(first, infoHash1);
   });
 
+  it("announce should emit TorrentAdded event", async () => {
+    const result = await this.tracker.announce(infoHash1, peer1, {from: id1});
+
+    eventEmitted(result, 'TorrentAdded', (ev) => {
+      return ev['infoHash'] === infoHash1;
+    }, 'TorrentAdded should be emitted with correct parameters');
+  });
+
   it("announce should add peer", async () => {
-    await this.tracker.announce(infoHash1, peer1);
+    await this.tracker.announce(infoHash1, peer1, {from: id1});
     const [first] = await this.tracker.peers(infoHash1);
-    assert.equal(first["peerId"], peerId1);
+    assert.equal(first["id"], id1);
+  });
+
+  it("announce should emit PeerUpdated event", async () => {
+    const result = await this.tracker.announce(infoHash1, peer1, {from: id1});
+
+    eventEmitted(result, 'PeerUpdated', (ev) => {
+      return ev['infoHash'] === infoHash1 &&
+        ev['id'] === id1;
+    }, 'PeerUpdated should be emitted with correct parameters');
   });
 
   it("announce with timed-out oldPeerId should replace peer", async () => {
     await this.tracker.setTimeout(0);
-    await this.tracker.announce(infoHash1, peer1);
-    await this.tracker.announce(infoHash1, peerId1, peer2);
+    await this.tracker.announce(infoHash1, peer1, {from: id1});
+    await this.tracker.methods['announce(bytes20,address,(address,bytes6,uint8,uint64,uint64,uint64,uint32))'](infoHash1, id1, peer2, {from: id2});
     const [first] = await this.tracker.peers(infoHash1);
-    assert.equal(first["peerId"], peerId2);
+    assert.equal(first["id"], id2);
+  });
+
+  it("announce with timed-out oldPeerId should emit PeerRemoved and PeerUpdated event", async () => {
+    await this.tracker.setTimeout(0);
+    await this.tracker.announce(infoHash1, peer1, {from: id1});
+    const result = await this.tracker.methods['announce(bytes20,address,(address,bytes6,uint8,uint64,uint64,uint64,uint32))'](infoHash1, id1, peer2, {from: id2});
+
+    eventEmitted(result, 'PeerUpdated', (ev) => {
+      return ev['infoHash'] === infoHash1 &&
+        ev['id'] === id2;
+    }, 'PeerUpdated should be emitted with correct parameters');
+
+    eventEmitted(result, 'PeerRemoved', (ev) => {
+      return ev['infoHash'] === infoHash1 &&
+        ev['id'] === id1;
+    }, 'PeerUpdated should be emitted with correct parameters');
   });
 
   it("announce with not (yet) timed-out oldPeerId should revert", async () => {
-    await this.tracker.announce(infoHash1, peer1);
-    await reverts(this.tracker.announce(infoHash1, peerId1, peer2))
+    await this.tracker.announce(infoHash1, peer1, {from: id1});
+    await reverts(this.tracker.methods['announce(bytes20,address,(address,bytes6,uint8,uint64,uint64,uint64,uint32))'](infoHash1, id1, peer2, {from: id2}))
   });
 
   it("announce with timed-out oldPeerId should be cheaper than announce", async () => {
     await this.tracker.setTimeout(0);
-    const gas1 = await this.tracker.announce.estimateGas(infoHash1, peer1);
-    await this.tracker.announce(infoHash1, peer1);
-    const gas2 = await this.tracker.announce.estimateGas(infoHash1, peerId1, peer2);
+    const gas1 = await this.tracker.announce.estimateGas(infoHash1, peer1, {from: id1});
+    await this.tracker.announce(infoHash1, peer1, {from: id1});
+    const gas2 = await this.tracker.methods['announce(bytes20,address,(address,bytes6,uint8,uint64,uint64,uint64,uint32))'].estimateGas(infoHash1, id1, peer2, {from: id2});
     assert.isBelow(gas2, gas1);
   });
 
   it("announce should ignore peer.updated and use block.timestamp instead", async () => {
     const before = Math.floor(Date.now() / 1000);
-    await this.tracker.announce(infoHash1, peer1);
+    await this.tracker.announce(infoHash1, peer1, {from: id1});
     const [first] = await this.tracker.peers(infoHash1);
     assert.isAtLeast(+first["updated"], before);
   });
 
   it("announce with timed-out oldPeerId should ignore peer.updated and use block.timestamp instead", async () => {
     await this.tracker.setTimeout(0);
-    await this.tracker.announce(infoHash1, peer1);
+    await this.tracker.announce(infoHash1, peer1, {from: id1});
     const before = Math.floor(Date.now() / 1000);
-    await this.tracker.announce(infoHash1, peerId1, peer2);
+    await this.tracker.methods['announce(bytes20,address,(address,bytes6,uint8,uint64,uint64,uint64,uint32))'](infoHash1, id1, peer2, {from: id2});
     const [first] = await this.tracker.peers(infoHash1);
     assert.isAtLeast(+first["updated"], before);
+  });
+
+  it("announce should ignore peer.id and use msg.sender instead", async () => {
+    const peer1 = [empty, compact, "0", "0", "0", "100", "0"];
+    await this.tracker.announce(infoHash1, peer1, {from: id1});
+    const [first] = await this.tracker.peers(infoHash1);
+    assert.equal(+first["id"], id1);
+  });
+
+  it("announce with timed-out oldPeerId should ignore peer.id and use msg.sender instead", async () => {
+    const peer2 = [empty, compact, "0", "0", "0", "100", "0"];
+    await this.tracker.setTimeout(0);
+    await this.tracker.announce(infoHash1, peer1, {from: id1});
+    await this.tracker.methods['announce(bytes20,address,(address,bytes6,uint8,uint64,uint64,uint64,uint32))'](infoHash1, id1, peer2, {from: id2});
+    const [first] = await this.tracker.peers(infoHash1);
+    assert.equal(+first["id"], id2);
   });
 });
